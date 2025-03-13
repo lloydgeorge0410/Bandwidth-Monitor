5afe256b6496879cfcfc945eb6060410b7dc82ee
lloydgeorge0410 <lloydgeorge0410@outlook.com>
2025-03-13 10:55:30 -0700

    pw-27: add user table

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import app from "./app";
+import { initializeDataSource } from "./database/dataSource";
 import { ENV } from "./utils/constants";
 import Logger from "./utils/logger";
 
 const startServer = async () => {
-  app.listen(ENV.PORT, ENV.HOST, () => {
-    Logger.info(`Server is running at http://${ENV.HOST}:${ENV.PORT}.`);
-  });
+  initializeDataSource().then(() => {
+    app.listen(ENV.PORT, ENV.HOST, () => {
+      Logger.info(`Server is running at http://${ENV.HOST}:${ENV.PORT}.`);
+    });
+  });  
 };
 
 startServer();
---
+++ backend/src/migrations/1741818706219-create-user-table.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUserTable1741818706219 implements MigrationInterface {
+    name = 'CreateUserTable1741818706219'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('admin', 'user')`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "password" character varying NOT NULL, "name" character varying NOT NULL, "ip" character varying NOT NULL, "limit" bigint, "role" "public"."users_role_enum" NOT NULL DEFAULT 'user', "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_51b8b26ac168fbe7d6f5653e6cf" UNIQUE ("name"), CONSTRAINT "UQ_da975f2f2d61d876ff98acff765" UNIQUE ("ip"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+    }
+
+}
